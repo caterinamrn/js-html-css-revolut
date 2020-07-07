ab94630086f8c0e0b29ac14764cbf61095c68717
caterina <catemarian1806@gmail.com>
2020-07-07 13:18:09 +0200

    tentativo dropdown scorrendo con il mouse

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  console.log("prova");
+  $(".dropdown_parent").mouseenter(function(){
+    $(this).children(".dropdown_content").show();
+    });
+
+    $(".dropdown_content").mouseleave(function(){
+    $(this).hide();
+    console.log("pipppo");
+  });
+
+  // $(".dropdown_parent").mouseleave(function(){
+  //   $(this).children(".dropdown_content").hide();
+  // });
 });
